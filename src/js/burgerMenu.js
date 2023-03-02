cc9eca3fe270076c103667c2c8249559ab664aaa
Oleksandr Sai <alexcdsay@gmail.com>
2023-03-02 17:29:00 +0200

    add adaptive menu

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,2 +1,7 @@
-let a = "Работает же"
-console.log(a)
+let burgerButton = document.querySelector(".header__burger");
+let headerMenu = document.querySelector(".header__menu")
+
+burgerButton.addEventListener("click", ()=>{
+    burgerButton.classList.toggle("active")
+    headerMenu.classList.toggle("activeMenu")
+})
