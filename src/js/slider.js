d76475b43a9a006a9d305e22a19c354c7c994107
Oleksandr Sai <alexcdsay@gmail.com>
2023-03-07 18:33:12 +0200

    add section home1

--- src/js/slider.js
+++ src/js/slider.js
@@ -4,24 +4,24 @@ let slide3 = document.querySelector("#r3");
 let slider = document.querySelector(".slider")
 
 slide1.checked = true;
-slider.style.marginLeft = "-200%"
 
-// stepSlide()
-// document.addEventListener("input", examination)
 
-//  function stepSlide() {
-//     setTimeout(()=> {slide1.checked = true; examination();
-//         setTimeout(()=> {slide2.checked = true; examination();
-//         setTimeout(()=> {slide3.checked = true; examination(); stepSlide()},5000)
-//         },5000)
-//         },5000)
-//     }
+stepSlide()
+document.addEventListener("input", examination)
 
-//  function examination (){
-//     if(slide1.checked) slider.style.marginLeft = "0%";
-//     if(slide2.checked) slider.style.marginLeft = "-100%";
-//     if(slide3.checked) slider.style.marginLeft = "-200%";   
-//  }
+ function stepSlide() {
+    setTimeout(()=> {slide1.checked = true; examination();
+        setTimeout(()=> {slide2.checked = true; examination();
+        setTimeout(()=> {slide3.checked = true; examination(); stepSlide()},5000)
+        },5000)
+        },5000)
+    }
+
+ function examination (){
+    if(slide1.checked) slider.style.marginLeft = "0%";
+    if(slide2.checked) slider.style.marginLeft = "-100%";
+    if(slide3.checked) slider.style.marginLeft = "-200%";   
+ }
    
 
  
